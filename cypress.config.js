a6141f11e846a4a8e556811e835b77878317b935
i-Gor <i.gorshchuk@gmail.com>
2024-03-16 20:04:36 +0200

    edited cypress.config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1920, 
+  viewportHeight: 1080,
+  watchForFileChanges: false,
+  retries: {
+      runMode: 2,
+      openMode: 2
+    },
+
   e2e: {  
-    viewportWidth: 1920, 
-    viewportHeight: 1080,
-    watchForFileChanges: false,
-    retries: {
-        runMode: 2,
-        openMode: 2
-      },
-  
+    baseUrl: 'https://juice-shop-sanitarskyi.herokuapp.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
